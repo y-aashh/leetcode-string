4f9f5ac3717b06d4b0ffaa699739d80e21a85c22
y-aashh <40933032+y-aashh@users.noreply.github.com>
2024-02-06 17:25:29 +0100

    Add files via upload

---
+++ leet.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None:
+            return head
+        
+        prev = None
+        curr = head
+
+        while curr:
+            n = curr.next
+            curr.next = prev
+            prev = curr
+            curr = n
+
+        return prev
